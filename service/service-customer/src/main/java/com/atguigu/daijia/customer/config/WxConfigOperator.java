6e8950497f08a45959e8bc48a6e582acf10a5815
chengsiliang <1127887913@qq.com>
2024-09-06 16:12:06 +0800

    乘客登录接口

---
+++ service/service-customer/src/main/java/com/atguigu/daijia/customer/config/WxConfigOperator.java
@@ -0,0 +1,23 @@
+package com.atguigu.daijia.customer.config;
+
+import cn.binarywang.wx.miniapp.api.WxMaService;
+import cn.binarywang.wx.miniapp.api.impl.WxMaServiceImpl;
+import cn.binarywang.wx.miniapp.config.impl.WxMaDefaultConfigImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WxConfigOperator {
+    @Autowired
+    private WxConfigProperties wxConfigProperties;
+    @Bean
+    public WxMaService wxMaService(){
+        WxMaDefaultConfigImpl wxMaDefaultConfig=new WxMaDefaultConfigImpl();
+        wxMaDefaultConfig.setAppid(wxConfigProperties.getAppId());
+        wxMaDefaultConfig.setAppid(wxConfigProperties.getSecret());
+        WxMaService service=new WxMaServiceImpl();
+        service.setWxMaConfig(wxMaDefaultConfig);
+        return service;
+    }
+}
--- service/service-customer/src/main/java/com/atguigu/daijia/customer/service/impl/CustomerInfoServiceImpl.java
+++ service/service-customer/src/main/java/com/atguigu/daijia/customer/service/impl/CustomerInfoServiceImpl.java
@@ -1,8 +1,13 @@
 package com.atguigu.daijia.customer.service.impl;
 
+
+import cn.binarywang.wx.miniapp.api.WxMaService;
 import com.atguigu.daijia.customer.mapper.CustomerInfoMapper;
+import com.atguigu.daijia.customer.mapper.CustomerLoginLogMapper;
 import com.atguigu.daijia.customer.service.CustomerInfoService;
 import com.atguigu.daijia.model.entity.customer.CustomerInfo;
+import com.atguigu.daijia.model.entity.customer.CustomerLoginLog;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,5 +17,43 @@ import org.springframework.stereotype.Service;
 @Service
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class CustomerInfoServiceImpl extends ServiceImpl<CustomerInfoMapper, CustomerInfo> implements CustomerInfoService {
+    @Autowired
+    private WxMaService wxMaService;
+    @Autowired
+    private CustomerInfoMapper customerInfoMapper;
 
+    @Autowired
+    private CustomerLoginLogMapper customerLoginLogMapper;
+    @Override
+    public Long login(String code) {
+        //1 获取code值，使用微信工具包对象，获取微信唯一标识openid
+        String openid = null;
+//        try {
+//            WxMaJscode2SessionResult sessionInfo =
+//                    wxMaService.getUserService().getSessionInfo(code);
+//            openid = sessionInfo.getOpenid();
+//        } catch (WxErrorException e) {
+//            throw new RuntimeException(e);
+//        }
+        openid=code;
+        //2 根据openid查询数据库表，判断是否第一次登录
+        //如果openid不存在返回null，如果存在返回一条记录
+        LambdaQueryWrapper<CustomerInfo> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(CustomerInfo::getWxOpenId,openid);
+        CustomerInfo customerInfo = customerInfoMapper.selectOne(wrapper);
+        //3 如果第一次登录，添加信息到用户表
+        if (customerInfo==null){
+            customerInfo = new CustomerInfo();
+            customerInfo.setNickname(String.valueOf(System.currentTimeMillis()));
+            customerInfo.setAvatarUrl("https://oss.aliyuncs.com/aliyun_id_photo_bucket/default_handsome.jpg");
+            customerInfo.setWxOpenId(openid);
+            customerInfoMapper.insert(customerInfo);
+        }
+        //4 记录登录日志信息
+        CustomerLoginLog customerLoginLog = new CustomerLoginLog();
+        customerLoginLog.setCustomerId(customerInfo.getId());
+        customerLoginLog.setMsg("小程序登录");
+        customerLoginLogMapper.insert(customerLoginLog);
+        return customerInfo.getId();
+    }
 }
